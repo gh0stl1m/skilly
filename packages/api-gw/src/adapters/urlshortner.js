a940f02c16a07cce1233b144cf8b3a7c0c35bfab
Santiago Sanchez Taborda <santiago.sanchez.t@gmail.com>
2020-12-23 03:45:49 -0500

    feat(API): Add service that allows an read the original URL

--- packages/api-gw/src/adapters/urlshortner.js
+++ packages/api-gw/src/adapters/urlshortner.js
@@ -1,4 +1,4 @@
-const { createShortUrl } = require('@skilly/urlshortner-module');
+const { createShortUrl, readUrlByHash } = require('@skilly/urlshortner-module');
 const { increaseUrlCounter } = require('@skilly/statistics-module');
 
 const { logger } = require('../utils');
@@ -37,8 +37,49 @@ const shortUrlGenerator = [
         entity: 'url',
       });
   },
+];
+
+const readOriginalUrl = [
+  async (ctx, next) => {
+    ctx.checkParams('hash')
+      .notEmpty();
+
+    return ctx.checkValidate(next);
+  },
+  async (ctx, next) => {
+    try {
+      return await next();
+    } catch (err) {
+      return ctx.serverResponse()
+        .addError({
+          message: err.message,
+        });
+    }
+  },
+  async (ctx) => {
+    const { hash } = ctx.params;
+
+    logger.info('[sk-api-gw]: Getting original URL');
+    const dataUrl = await readUrlByHash(hash, { originalUrl: 1 });
+
+    if (!dataUrl) {
+      logger.error('[sk-api-gw]: URL not found');
+      return ctx.serverResponse()
+        .addError({
+          statusCode: 404,
+          message: "The URL does not exists",
+        });
+    };
+
+    increaseUrlCounter(dataUrl._id)
+      .then(() => logger.info('[sk-api-gw]: URL usage reported success'))
+      .catch(err => logger.error(`[sk-api-gw]: Something went wrong reporting statistics ${err.message}`));
+
+    return ctx.redirect(dataUrl.originalUrl);
+  }
 ]
 
 module.exports = {
   shortUrlGenerator,
+  readOriginalUrl,
 };
--- packages/api-gw/src/delivery/http/routes/urlshortner/endpoints.js
+++ packages/api-gw/src/delivery/http/routes/urlshortner/endpoints.js
@@ -2,12 +2,11 @@ const Router = require('koa-router');
 
 const { urlShortner } = require('../../../../adapters');
 
-const router = new Router({
-  prefix: '/',
-});
+const router = new Router();
 
 module.exports = () => {
   router.post('/', ...urlShortner.shortUrlGenerator);
+  router.get('/:hash', ...urlShortner.readOriginalUrl);
 
   return router;
 };
